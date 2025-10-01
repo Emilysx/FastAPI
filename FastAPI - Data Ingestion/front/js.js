9f4ec3e4a13827997190cfb13cf70314a9c711de
Emilysx <emilyg.oliveira27@gmail.com>
2025-10-01 09:48:37 -0300

    Api conectada com o banco de dados - 01/10/2025

--- FastAPI - Data Ingestion/front/js.js
+++ FastAPI - Data Ingestion/front/js.js
@@ -1,38 +1,52 @@
+// // Função assíncrona para puxar dados da API
 // async function puxando_api() {
+//     // Faz a requisição GET para a API
 //     const response = await fetch("http://localhost:8000/api/v1/bandas/");
+
+//     // Retorna os dados da resposta em formato JSON
 //     const data = await response.json();
-//     return data;
+//     return data; // Retorna os dados que serão usados no frontend
     
 // }
 
+// // Função assíncrona para mostrar as bandas no HTML
 // async function mostrar_banda() {
 //     const bandas = await puxando_api();
-//     const container = document.getElementById("bandas-container");
+//     const container = document.getElementById("bandas-conteiner"); // Seleciona o container para inserir as bandas
 
+//     // Loop para cada banda, cria o HTML e insere no container
 //     bandas.forEach(banda => {
+//         // Cria uma div para cada banda
 //         const bandaDiv = document.createElement('div');
 //         bandaDiv.classList.add('banda');
+
+//         // Preenche a div com informações sobre a banda
 //         bandaDiv.innerHTML = `
 //         <h2> ${banda.nome}</h2>
 //         <p>${banda.qtd_integrantes}</p>
 //         <p>${banda.tipo_musical}</p>
 //         `;
-
 //         container.appendChild(bandaDiv);
  
-//     });
-    
+//     });   
 // }
 
 // mostrar_banda()
 
+
+// Função assíncrona para puxar dados da API usando Axios
 async function puxar_api() {
+    // Faz a requisição GET para a API
     await axios.get("http://localhost:8000/api/v1/bandas/").then((response) =>{
         const bandas = response.data;
         const container = document.getElementById("bandas-conteiner");
+
+        // Loop para cada banda e cria um novo elemento no HTML
         bandas.forEach(element => {
             const bandaDiv = document.createElement('div');
             bandaDiv.classList.add('banda');
+
+            // Preenche a div com as informações da banda
             bandaDiv.innerHTML= `
             <h2>${element.nome}</h2>
             <p>${element.qtd_integrantes}</p>
--- FastAPI - Data Ingestion/main.py
+++ FastAPI - Data Ingestion/main.py
@@ -5,7 +5,7 @@ from api.v1.api import api_router
 
 app = FastAPI(title="API das bandas da DS18")
 
-origins = ["http://localhost", "http://localhost:8080", "http://localhost:5500", "http;//127.0.0.1:5500"]
+origins = ["http://localhost", "http://localhost:8080", "http://localhost:5500", "http://127.0.0.1:5500"]
 
 app.add_middleware(CORSMiddleware, allow_origins= origins, allow_credentials = True, allow_methods=["*"], allow_headers=["*"])
 
